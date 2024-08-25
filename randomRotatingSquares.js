f563551cbad75819bb809e75a2e8775054de4d58
“tanya-ermlva” <hi@ermolaeva.co>
2024-08-25 19:49:26 +0100

    added cdn p5 and different rotation directions for rotatingSquares sketch

--- randomRotatingSquares.js
+++ randomRotatingSquares.js
@@ -2,16 +2,20 @@ const bgColor = 245;
 
 
 function setup() {
-    createCanvas(680, 1100);
-    background(bgColor);
+    createCanvas(680, 1250);
+    // background(bgColor);
     noFill();
     stroke('black');
     rectMode(CENTER);
     angleMode(DEGREES);
-    squaresGrid();
+    // squaresGrid();
+    frameRate(10);
 };
 
 function draw() {
+    background(bgColor);
+    squaresGrid();
+
     
 
 };
@@ -39,6 +43,12 @@ const squaresGrid = () => {
             
   
             let angle = map(sY, 0, height, 0, random(45, 90));
+
+            // Randomly decide if the rotation should be clockwise or counterclockwise
+            let direction = random([-1, 1]); 
+            
+            // Apply the direction to the angle
+            angle *= direction;
 
             
             console.log(`this is the current Y ${sY}`);
